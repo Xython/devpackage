d99bcfcb6797de546f5fcdfec10046575af15d0b
thautwarm <twshere@outlook.com>
2019-03-16 17:04:06 +0800

    test: inprocess setup

--- test.py
+++ test.py
@@ -3,17 +3,23 @@ from devpackage.disttools.path import Path
 from os import chdir, mkdir
 from subprocess import check_output
 from shutil import rmtree
+import sys
 
 mkdir('./test_sample')
 chdir('./test_sample')
 
 init("mock_pack1", license="MIT")
 init("mock_pack2_qwertyuiopasdfghjkl", license="bsd3", md=True)
-
 chdir('./mock_pack2_qwertyuiopasdfghjkl')
-check_output(['python', 'setup.py', 'install'])
-clean()
-check_output(['pip', 'uninstall', '-y', 'mock_pack2_qwertyuiopasdfghjkl'])
 
+argv = sys.argv
+argv.clear()
+argv.extend([str(Path("setup.py")), 'install'])
+
+with open('setup.py', 'r', encoding='utf8') as f:
+    exec(f.read())
+clean()
+
+check_output(['pip', 'uninstall', '-y', 'mock_pack2_qwertyuiopasdfghjkl'])
 chdir('../../')
 rmtree('./test_sample')
